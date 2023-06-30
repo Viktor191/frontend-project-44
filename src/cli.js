3ec09dfe55e7e71ace21dbf806186997a9b762e0
Viktor Denisov <viktor@192.168.1.12>
2023-06-30 16:19:48 +0200

    added cli.js file and readlineSync module

---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync';
+export const askNameGreeting = () => {
+
+    // Wait for user's response.
+    let userName = readlineSync.question('May I have your name? ');
+    console.log('Hi ' + userName + '!');
+}
