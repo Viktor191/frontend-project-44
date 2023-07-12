c2722638700747d6023737895c7163488c795e41
Viktor Denisov <viktor@192.168.1.12>
2023-07-12 16:22:48 +0200

    recurring functions moved to file index.js

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import readlineSync from 'readline-sync';
+export const askNameGreeting = () => {
+    let name;
+    console.log('Welcome to the Brain Games!')
+    var userName = readlineSync.question('May I have your name? ');
+    console.log('Hi, ' + userName + '!');
+    name = userName
+    return name
+}
+export const getRandomInt = () => {
+    let min = 1;
+    let max = 101;
+    return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+}
+export const getRandomIntSign = () => {
+    let min = 0;
+    let max = 3;
+    let arrSign = ['+', '*', '-'];
+    let namberSign = Math.floor(Math.random() * (max - min)) + min;
+    return arrSign[namberSign];
+    //Максимум не включается, минимум включается
+}
