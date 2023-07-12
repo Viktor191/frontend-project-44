e87c562122c0df56643c667cf23541916a801ed7
Viktor Denisov <viktor@192.168.1.12>
2023-07-12 16:20:35 +0200

    moved functions to one file index.js

--- src/askNameGreeting.js
+++
@@ -1,7 +0,0 @@
-import readlineSync from 'readline-sync';
-export const askNameGreeting = () => {
-    console.log('Welcome to the Brain Games!')
-    var userName = readlineSync.question('May I have your name? ');
-    console.log('Hi, ' + userName + '!');
-    return userName
-}
