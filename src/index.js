407a43e48c8d48e0ce2fc22c3b8adc3f2b7e7a23
Viktor Denisov <viktor@192.168.1.12>
2023-07-18 18:29:49 +0200

    added a function to generate a random array getRandomArr

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import readlineSync from 'readline-sync';
+
 export const askNameGreeting = () => {
     let name;
-    console.log('Welcome to the Brain Games!')
+    console.log('Welcome to the Brain Games!');
     var userName = readlineSync.question('May I have your name? ');
     console.log('Hi, ' + userName + '!');
-    name = userName
-    return name
+    name = userName;
+    return name;
 }
 export const getRandomInt = () => {
     let min = 1;
     let max = 101;
     return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
-}
+};
 export const getRandomIntSign = () => {
     let min = 0;
     let max = 3;
@@ -20,13 +21,31 @@ export const getRandomIntSign = () => {
     return arrSign[namberSign];
     //Максимум не включается, минимум включается
 }
-export function NOD () {
+
+export function NOD() {
     for (var x = arguments[0], i = 1; i < arguments.length; i++) {
         var y = arguments[i];
         while (x && y) {
             x > y ? x %= y : y %= x;
-        }
+        };
         x += y;
     }
     return x;
 }
+export const getRandomArr = () => {
+    let min = 5;
+    let max = 11;
+    let arr = [];
+    let result = getRandomInt();
+    let progressionStep = getRandomInt();
+
+    let maxLengthArr = Math.floor(Math.random() * (max - min)) + min;
+    arr.push(result);
+
+    for (let i = 0; i < maxLengthArr - 1; i++) {
+        result = result + progressionStep;
+        arr.push(result);
+    };
+    return arr;
+    //Максимум не включается, минимум включается
+};
