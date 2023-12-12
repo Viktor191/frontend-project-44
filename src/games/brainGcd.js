0868a909b4d11a00a233dbb9adfe345cc2223993
Viktor Denisov <viktor@192.168.1.9>
2023-12-12 19:05:59 +0100

    changing the structure of the Gcd game

--- src/games/brainGcd.js
+++ src/games/brainGcd.js
@@ -1,27 +1,20 @@
-import readlineSync from 'readline-sync';
-import { getRandomInt, askNameGreeting, NOD } from '../index.js';
+import { getGameLaunchTemplate, getRandomInt, NOD } from '../index.js';
 
-const localUserName = askNameGreeting();
+const getOneRoundPlay = () => {
+  const GAME_INFO = {};
+  const randomNam1 = getRandomInt();
+  const randomNam2 = getRandomInt();
+
+  GAME_INFO.correctAnswer = NOD(randomNam1, randomNam2);
+  GAME_INFO.questionOfRound = `${randomNam1} ${randomNam2}`;
+
+  console.log(`Подсказка для тестов ${GAME_INFO.correctAnswer}`);
+
+  return GAME_INFO;
+};
 const brainGcd = () => {
-  let randomNam1;
-  let randomNam2;
-  let result;
-  console.log('Find the greatest common divisor of given numbers.');
-  for (let i = 0; i < 3; i += 1) {
-    randomNam1 = getRandomInt();
-    randomNam2 = getRandomInt();
-    result = NOD(randomNam1, randomNam2);
-    console.log(`Question: ${randomNam1} ${randomNam2}`);
-    // console.log('Подсказка для тестов' + result);
-    const userAnswer = readlineSync.question('Your answer: ');
-    if (Number(userAnswer) === result) {
-      console.log('Correct!');
-    } else {
-      console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${result}".
-        Let's try again, ${localUserName}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${localUserName}!`);
+  const gameDescription = 'Find the greatest common divisor of given numbers.';
+
+  getGameLaunchTemplate(getOneRoundPlay, gameDescription);
 };
 export default brainGcd;
