fe760ea41d691d6b2466555c81e2ded320ad504d
Viktor Denisov <viktor@192.168.1.9>
2023-12-20 17:58:01 +0100

    added the ability to set ranges in getRandomInt

--- src/util.js
+++ src/util.js
@@ -1,7 +1,22 @@
+import readlineSync from 'readline-sync';
+/*
 const getRandomInt = () => {
   const min = 1;
   const max = 101;
 
   return Math.floor(Math.random() * (max - min)) + min;
 };
+*/
+
+const getRandomInt = () => {
+  console.log('Set the border settings for random number generation.');
+
+  const min = readlineSync.question('Enter the lower limit of the range:');
+  const max = readlineSync.question('Enter the upper limit of the range:');
+
+  console.log(`Specified range from ${min} to ${max}`);
+
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
 export default getRandomInt;
